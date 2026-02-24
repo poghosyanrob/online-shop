8f9fc0bdb5e8054bd8605cdaa4375d2b39b49501
Robert Poghosyan <robert.poghosyan.g@gmail.com>
2026-02-24 23:59:09 +0400

    master do changes

--- src/main/java/am/example/onlineshop/controller/PaymentController.java
+++ src/main/java/am/example/onlineshop/controller/PaymentController.java
@@ -1,12 +1,13 @@
 package am.example.onlineshop.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PaymentController {
 
-    @PostMapping("/pay")
+    @RequestMapping(value = "/pay", method = RequestMethod.GET)
     public String pay(){
         return "pay";
     }
